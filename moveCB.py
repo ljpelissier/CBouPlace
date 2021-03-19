88e1359b1cca17644e69b4ba3a145caff76f3b74
ljpelissier <l.j.pelissier+github@gmail.com>
2021-03-19 12:16:12 -0400

    first commit of python file.

---
+++ moveCB.py
@@ -0,0 +1,6 @@
+# python script will read year, grade and chromebook serial number from csv
+# using the year and grade level will calculate graduation year and place
+# the chromebook in the corresponding ou using GAM
+# server location and school name will be contaned int config.txt
+
+
