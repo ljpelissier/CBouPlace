12705d5d9437bc855fc76d6934c98ffe2e43c8e4
Louis Pelissier <l.j.pelissier+github@gmail.com>
2021-03-19 20:47:20 -0400

    gam command created in dataframe

--- moveCB.py
+++ moveCB.py
@@ -10,5 +10,13 @@ import pandas as pd
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+
 dataFrame =  pd.read_csv(config['hostpath']['hostPath'] + config['hostpath']['fileName'], index_col='StudentID')
 dataFrame['CBSerialNumber'] = dataFrame['CBSerialNumber'].str.split(' ', expand=False).str[0]
+dataFrame['GradeLevel'] = dataFrame['GradeLevel'].replace('KF','10')
+dataFrame['GradYear'] = dataFrame['SchoolYear'].str[:4].astype('int') + 9 - dataFrame['GradeLevel'].astype('int')
+dataFrame['gamCommand'] = 'gam cros_sn ' + dataFrame['CBSerialNumber'] + ' update ou ' + dataFrame['GradeLevel'].map(config['ouRoot']) + 'ClassOf' + dataFrame['GradYear'].astype('str')
+
+header = ["SchoolYear", "GradeLevel", "CBSerialNumber", "gamCommand"]
+dataFrame.to_csv('output.ini', columns = header)
+
