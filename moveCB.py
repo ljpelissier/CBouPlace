4806678700a31cb33673419f5243dd853c150afb
Louis Pelissier <l.j.pelissier+github@gmail.com>
2021-03-19 21:45:49 -0400

    Working concept

--- moveCB.py
+++ moveCB.py
@@ -1,22 +1,34 @@
-# python script will read year, grade and chromebook serial number from csv
-# using the year and grade level will calculate graduation year and place
-# the chromebook in the corresponding ou using GAM
-# server location and school name will be contaned int config.txt
-
+"""
+ author: Louis Pelssier
+ python script will read year, grade and chromebook serial number from csv
+ using the year and grade level will calculate graduation year and place
+ the chromebook in the corresponding ou using GAM
+ server location and school name will be contaned int config.ini
+ see readme for details on config.ini
+"""
 import configparser
 import os
 import pandas as pd
 
+#year offset based on grade level
+YEAROFFSET = 9
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-
-dataFrame =  pd.read_csv(config['hostpath']['hostPath'] + config['hostpath']['fileName'], index_col='StudentID')
+dataFrame =  pd.read_csv(config['hostpath']['hostPath'] + \
+                        config['hostpath']['fileName'], index_col='StudentID')
 dataFrame['CBSerialNumber'] = dataFrame['CBSerialNumber'].str.split(' ', expand=False).str[0]
 dataFrame['GradeLevel'] = dataFrame['GradeLevel'].replace('KF','10')
-dataFrame['GradYear'] = dataFrame['SchoolYear'].str[:4].astype('int') + 9 - dataFrame['GradeLevel'].astype('int')
-dataFrame['gamCommand'] = 'gam cros_sn ' + dataFrame['CBSerialNumber'] + ' update ou ' + dataFrame['GradeLevel'].map(config['ouRoot']) + 'ClassOf' + dataFrame['GradYear'].astype('str')
+dataFrame['GradYear'] = dataFrame['SchoolYear'].str[:4].astype('int') +\
+                        YEAROFFSET - dataFrame['GradeLevel'].astype('int')
+dataFrame['gamCommand'] = 'gam cros_sn ' + dataFrame['CBSerialNumber'] +\
+                          ' update ou ' + dataFrame['GradeLevel'].map(config['ouRoot']) +\
+                          'ClassOf' + dataFrame['GradYear'].astype('str')
 
-header = ["SchoolYear", "GradeLevel", "CBSerialNumber", "gamCommand"]
-dataFrame.to_csv('output.ini', columns = header)
-
+for gamCommand in dataFrame['gamCommand']:
+    if isinstance(gamCommand,str):
+        #print(gamCommand) #uncomment this line to see the gam command
+                           #comment out line below to run as test
+        os.system(gamCommand)
+  
