e113f6df6ea4044e674482f167c800e2c42ec918
ljpelissier <l.j.pelissier+github@gmail.com>
2021-11-02 14:00:06 -0400

    fixed Kindergarten grade

--- moveCB-CONFLICT-1.py
+++ moveCB-CONFLICT-1.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
  author: Louis Pelssier
  python script will read year, grade and chromebook serial number from csv
@@ -10,7 +9,6 @@
 import configparser
 import os
 import pandas as pd
-import subprocess
 
 #year offset based on grade level
 #comment line 34 and uncomment line 32 to test
@@ -22,26 +20,16 @@ config.read('config.ini')
 dataFrame =  pd.read_csv(config['hostpath']['hostPath'] + \
                         config['hostpath']['fileName'], index_col='StudentID')
 dataFrame['CBSerialNumber'] = dataFrame['CBSerialNumber'].str.split(' ', expand=False).str[0]
-dataFrame['GradeLevel'] = dataFrame['GradeLevel'].replace('KF','0')
+dataFrame['GradeLevel'] = dataFrame['GradeLevel'].replace('KF','10')
 dataFrame['GradYear'] = dataFrame['SchoolYear'].str[:4].astype('int') +\
                         YEAROFFSET - dataFrame['GradeLevel'].astype('int')
 dataFrame['gamCommand'] = 'gam cros_sn ' + dataFrame['CBSerialNumber'] +\
                           ' update ou ' + dataFrame['GradeLevel'].map(config['ouRoot']) +\
                           'ClassOf' + dataFrame['GradYear'].astype('str')
 
-#for gamCommand in dataFrame['gamCommand']:
-#    if isinstance(gamCommand,str):
-#        #print(gamCommand) #uncomment this line to see the gam command
-#                           #comment out line below to run as test
-#        os.system(gamCommand)
-
-gamCommand = 'gam cros_ou "/Chromebooks/StudentChromebooks/Forrestdale/ClassOf2021" print cros fields serialnumber'
-#chromebooksInOU = []
-serialnumbersInOU = []
-chromebooksInOU = subprocess.check_output(gamCommand)
-print(type(chromebooksInOU))
-print('printing ou data')
-for cb in str(chromebooksInOU).split("\\r\\n"):
-    serialnumbersInOU.append(cb.split(","))
-for sn in serialnumbersInOU:
-    print(sn[1])
+for gamCommand in dataFrame['gamCommand']:
+    if isinstance(gamCommand,str):
+        #print(gamCommand) #uncomment this line to see the gam command
+                           #comment out line below to run as test
+        os.system(gamCommand)
+  
